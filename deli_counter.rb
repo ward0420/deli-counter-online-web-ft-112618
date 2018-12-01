46f0ac27a704c1744411e597d59a566a2f9e393e
Don Ward <10is0420@gmail.com>
2018-12-01 23:00:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,38 @@
-# Write your code here.
+def line()
+  
+  if katz_deli = ""
+    
+    puts "The line is currently empty."
+  
+  else 
+  
+    puts "The line is currently"
+  
+  end
+
+end
+    
+def take_a_number(katz_deli, name)
+  
+  position = 1
+  
+  name.each do |ticket|
+  
+  puts "Welcome, #{ticket}. You are number #{position} in line"
+  
+  position += 1
+  
+end
+
+end
+
+  #katz_deli = []
+  #other_deli = []
+  #another_deli =[]
+  
+ 
+  
+  
+
+  
+  
