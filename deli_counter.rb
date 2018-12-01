55e247138ff4b6ea3431f66a80bd34ccbcc6ed31
Don Ward <10is0420@gmail.com>
2018-12-01 22:36:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def line(katz_deli)
+    
+  puts "The line is currently empty."
+  
+end
+    
+def take_a_number(katz_deli, name)
+  
+    if katz_deli != ""
+
+    "Welcome, #{name}. You are number #{line_position} in line."
+    
+  end
+  
+end
+  
+  
+  
+
+  
+  
